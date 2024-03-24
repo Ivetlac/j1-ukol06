45ec46ac1f30f84b3b643478a73c198e0abac963
Iveta Lackova <lackova.yvette@gmail.com>
2024-03-24 17:59:48 +0100

    uloha

--- src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
@@ -5,23 +5,23 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SvatkySluzba {
 
-    private final ObjectMapper objectMapper = JsonMapper.builder()
-            .addModule(new JavaTimeModule())
-            .build();
+    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
     private final Path cestaKDatum = Path.of("data/svatky.json");
     private final SeznamSvatku seznamSvatku;
 
     public SvatkySluzba() throws IOException {
-        // TODO načíst seznam svátků ze souboru svatky.json
-
-        // Následující řádek po vlastní implementaci smažete.
-        seznamSvatku = null;
+        String json = Files.readString(cestaKDatum);
+        seznamSvatku = objectMapper.readValue(json, SeznamSvatku.class);
     }
 
     public List<String> vyhledatSvatkyDnes() {
@@ -29,14 +29,9 @@ public class SvatkySluzba {
     }
 
     public List<String> vyhledatSvatkyKeDni(MonthDay day) {
-        // TODO
-        // získat seznam svátků
-        // převést na Stream
-        // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
-        // pomocí metody map() získat z objektu jméno
-        // pomocí toList() převést na List
-
-        // Následující řádek po vlastní implementaci smažete.
-        return List.of();
+        return seznamSvatku.getSvatky().stream()
+                .filter(svatek -> svatek.getDen().equals(day))
+                .map(Svatek::getJmeno)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -1,13 +1,43 @@
 package cz.czechitas.ukol06.svatky;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.MonthDay;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
 
     @Test
-    void vyhledatSvatkyKeDni() {
-        //TODO implementovat test metody vyhledatSvatkyKeDni
+    void vyhledatSvatkyKeDni() throws IOException {
+        SvatkySluzba sluzba = new SvatkySluzba();
+
+        List<String> expected = List.of("Karina", "Vasil");
+        List<String> actual = sluzba.vyhledatSvatkyKeDni(MonthDay.of(1, 2));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSvatkyNaDatumBezSvatku() throws IOException {
+
+        SvatkySluzba sluzba = new SvatkySluzba();
+
+        List<String> svatky = sluzba.vyhledatSvatkyKeDni(MonthDay.of(1, 1));
+
+        assertEquals(List.of(), svatky);
+
+    }
+
+    @Test
+    void testSvatkyNaDatumJednoJmeno() throws IOException {
+        SvatkySluzba sluzba = new SvatkySluzba();
+
+        List<String> svatky = sluzba.vyhledatSvatkyKeDni(MonthDay.of(1, 4));
+
+
+        assertEquals(List.of("Diana"), svatky);
     }
 }
